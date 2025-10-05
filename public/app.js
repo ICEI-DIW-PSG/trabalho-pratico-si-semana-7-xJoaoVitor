73105e2cb49257d8be3a46f83c5e8594005418a1
João Vítor Rodrigues <joao.silva.1632009@sga.pucminas.br>
2025-10-05 16:33:52 -0300

    segundo commit

---
+++ public/app.js
@@ -0,0 +1,59 @@
+const eventos = [
+  {
+    id: 1,
+    titulo: "Santa Ceia do Senhor",
+    data: "Todo primeiro domingo do mês.",
+    descricaoLonga: "A santa ceia é um momento de profunda comunhão e reflexão, onde relembramos o sacrifício de Jesus Cristo por nós. Participe conosco deste ato de fé e adoração. Todos são bem-vindos para celebrar a nova aliança no sangue de Cristo.",
+    imagem: "img/santaceia.jpg",
+    horario: "19:00",
+    local: "Igreja Betesda"
+  },
+  {
+    id: 2,
+    titulo: "Batismo nas Águas",
+    data: "15 de Novembro de 2025",
+    descricaoLonga: "O batismo é um passo fundamental na jornada cristã, simbolizando a morte para o pecado e a ressurreição para uma nova vida em Jesus. Será um dia de grande festa e celebração. Se você deseja ser batizado, entre em contato com a liderança da igreja.",
+    imagem: "img/batismo.jpg",
+    horario: "09:00",
+    local: "Sítio da Volta"
+  }
+];
+
+// Função para montar a página de detalhes.
+function montarDetalhes() {
+
+    const container = document.getElementById('detalhe-container');
+    const params = new URLSearchParams(window.location.search);
+    const eventoId = params.get('id');
+    const evento = eventos.find(e => e.id === parseInt(eventoId));
+
+    if (evento) {
+        document.title = evento.titulo;
+
+        container.innerHTML = `
+            <div class="row g-5">
+                <div class="col-lg-6">
+                    <img src="${evento.imagem}" alt="${evento.titulo}" class="img-fluid rounded shadow-sm detalhe-img">
+                </div>
+                <div class="col-lg-6 d-flex flex-column">
+                    <h2 class="detalhe-titulo mb-3">${evento.titulo}</h2>
+                    <div class="detalhe-info">
+                        <p><strong>Data:</strong> ${evento.data}</p>
+                        <p><strong>Horário:</strong> ${evento.horario}</p>
+                        <p><strong>Local:</strong> ${evento.local}</p>
+                    </div>
+                    <p class="mt-3">${evento.descricaoLonga}</p>
+                    <div class="mt-auto">
+                      <a href="index.html#eventos" class="btn btn-voltar">Voltar</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    } 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.getElementById('detalhe-container')) {
+        montarDetalhes();
+    }
+});
